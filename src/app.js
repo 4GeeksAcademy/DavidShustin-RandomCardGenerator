a484b7a9d942a85775f8945d4509e298389af820
DavidShustin <167934750+DavidShustin@users.noreply.github.com>
2024-07-27 22:11:17 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function getRandomCard() {
+    const randomSuits = Math.floor(Math.random() * suits.length);
+    const randomNumbers = Math.floor(Math.random() * numbers.length);
+
+    document.querySelector(".top-suit").innerHTML = suits[randomSuits];
+    document.querySelector(".bottom-suit").innerHTML = suits[randomSuits];
+    document.querySelector(".number").innerHTML = numbers[randomSuits];
+
+    const isRed =
+      suits[randomSuits] === "&hearts;" || suits[randomSuits] === "&diams;";
+    document
+      .querySelectorAll(".top-suit, .bottom-suit, .number")
+      .forEach(element => {
+        element.style.color = isRed ? "#dc143c" : "";
+      });
+  }
+  getRandomCard();
+  document.querySelector("button").addEventListener("click", getRandomCard);
+
+  console.log("Thank you for helping me!");
 };
